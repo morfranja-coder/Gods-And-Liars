89d0898354baf40187e4f3639dfb36251c4b2127
morfranja-coder <morfranja@gmail.com>
2026-08-20 17:11:51 -0300

    test: cover phase 2 lobby ready rules

---
+++ tests/unit/test_lobby_ready.gd
@@ -0,0 +1,34 @@
+class_name LobbyReadyTest
+extends GdUnitTestSuite
+
+func before_test() -> void:
+	NetworkManager.reset()
+
+func after_test() -> void:
+	NetworkManager.reset()
+
+func test_ready_requires_two_players() -> void:
+	NetworkManager.register_peer(1, 1001, "Host")
+	NetworkManager.set_peer_ready(1, true)
+	assert_bool(NetworkManager.all_peers_ready()).is_false()
+
+func test_all_players_ready_returns_true() -> void:
+	NetworkManager.register_peer(1, 1001, "Host")
+	NetworkManager.register_peer(2, 1002, "Guest")
+	NetworkManager.set_peer_ready(1, true)
+	NetworkManager.set_peer_ready(2, true)
+	assert_bool(NetworkManager.all_peers_ready()).is_true()
+
+func test_unready_player_blocks_start_gate() -> void:
+	NetworkManager.register_peer(1, 1001, "Host")
+	NetworkManager.register_peer(2, 1002, "Guest")
+	NetworkManager.set_peer_ready(1, true)
+	NetworkManager.set_peer_ready(2, false)
+	assert_bool(NetworkManager.all_peers_ready()).is_false()
+
+func test_disconnect_removes_player_from_roster() -> void:
+	NetworkManager.register_peer(1, 1001, "Host")
+	NetworkManager.register_peer(2, 1002, "Guest")
+	NetworkManager.unregister_peer(2)
+	assert_int(NetworkManager.peers.size()).is_equal(1)
+	assert_bool(NetworkManager.peers.has(2)).is_false()

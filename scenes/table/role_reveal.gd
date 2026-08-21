ca01d81afc72c08b8d793ba24820f1232da99fdf
morfranja-coder <morfranja@gmail.com>
2026-08-21 16:21:46 -0300

    Refresh role reveal with heretic teammate

--- scenes/table/role_reveal.gd
+++ scenes/table/role_reveal.gd
@@ -9,11 +9,16 @@ func _ready() -> void:
 	panel.visible = false
 	close_button.pressed.connect(_on_close_pressed)
 	MatchAuthority.private_role_received.connect(_on_private_role_received)
+	MatchAuthority.private_heretic_teammate_received.connect(_on_private_heretic_teammate_received)
 	if MatchAuthority.local_role != PlayerState.Role.UNASSIGNED:
 		_show_local_role()
 
 func _on_private_role_received(_role: int) -> void:
 	_show_local_role()
+
+func _on_private_heretic_teammate_received(_peer_id: int, _display_name: String) -> void:
+	if MatchAuthority.local_role == PlayerState.Role.HERETIC:
+		_show_local_role()
 
 func _show_local_role() -> void:
 	role_label.text = MatchAuthority.role_title()

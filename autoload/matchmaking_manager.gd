23645b9855fabf06cfad664888d657a99a474b8d
morfranja-coder <morfranja@gmail.com>
2026-08-21 11:03:16 -0300

    Fix MatchmakingManager strict typing for Godot 4.7 CI

--- autoload/matchmaking_manager.gd
+++ autoload/matchmaking_manager.gd
@@ -17,8 +17,8 @@ var current_distance_tier: int = QuickMatchRules.DISTANCE_CLOSE
 func _process(_delta: float) -> void:
 	if state != STATE_SEARCHING:
 		return
-	var elapsed_ms := max(0, Time.get_ticks_msec() - queue_started_ms)
-	var next_tier := QuickMatchRules.distance_tier_for_elapsed(elapsed_ms)
+	var elapsed_ms: int = maxi(0, Time.get_ticks_msec() - queue_started_ms)
+	var next_tier: int = QuickMatchRules.distance_tier_for_elapsed(elapsed_ms)
 	if next_tier != current_distance_tier:
 		current_distance_tier = next_tier
 		search_scope_changed.emit(current_distance_tier)
@@ -72,7 +72,7 @@ func search_scope_name() -> String:
 func consider_candidates(candidates: Array[Dictionary]) -> Array[int]:
 	if state != STATE_SEARCHING:
 		return []
-	var party_ids := QuickMatchRules.find_exact_fit(local_party_size, candidates)
+	var party_ids: Array[int] = QuickMatchRules.find_exact_fit(local_party_size, candidates)
 	if party_ids.is_empty() and slots_needed() > 0:
 		return []
 	state = STATE_MATCH_FOUND

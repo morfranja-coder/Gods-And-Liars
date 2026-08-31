8fce856e95d5481ccb98f8de1e680b9b4545f8cc
morfranja-coder <morfranja@gmail.com>
2026-08-31 11:11:28 -0300

    Add main menu settings entry

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -1,17 +1,24 @@
 extends Control
 
 const LOBBY_SCENE := "res://scenes/lobby/lobby.tscn"
+const SETTINGS_SCENE := "res://scenes/settings/settings_menu.tscn"
 
 @onready var enter_button: Button = %EnterButton
+@onready var options_button: Button = %OptionsButton
 @onready var quit_button: Button = %QuitButton
 
 func _ready() -> void:
 	enter_button.pressed.connect(_on_enter_pressed)
+	options_button.pressed.connect(_on_options_pressed)
 	quit_button.pressed.connect(_on_quit_pressed)
 	enter_button.grab_focus()
 
 func _on_enter_pressed() -> void:
 	get_tree().change_scene_to_file(LOBBY_SCENE)
+
+func _on_options_pressed() -> void:
+	get_tree().root.set_meta("settings_return_scene", scene_file_path)
+	get_tree().change_scene_to_file(SETTINGS_SCENE)
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
